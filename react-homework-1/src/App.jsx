9ce68520befaf41f6cb687ac5ef30324b1ae689a
Khes Adderley <khesadderley@khess-macbook-air-1.home>
2023-06-10 15:30:09 +0200

    first commit

---
+++ react-homework-1/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Button from "./components/Button"
+
+class App extends Component {
+  state = {  } 
+  render() { 
+    return (
+      <><p>Welcome to Khes' Reactor</p>
+      <Button />
+      </>
+    );
+  }
+}
+ 
+export default App;
---
+++ react-homework-1/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+    state = { aboveAge: yes } 
+    render() { 
+        return (
+            <button>Button 1</button>
+        );
+    }
+}
+ 
+export default Button;
